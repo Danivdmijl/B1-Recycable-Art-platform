8541ca8077616cadcfa957c172d4019819c9cf41
Dani <90690863+Danivdmijl@users.noreply.github.com>
2025-05-13 10:26:07 +0200

    Added setup review panel plugin

---
+++ review-panel/review-panel.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Account Review Panel
+ * Plugin URI: https://www.linkedin.com/in/dani-van-der-mijl/
+ * Description: A plugin that takes all the new registerd users and put them in a review queue
+ * Version: 1.0
+ * Author: Dani van der Mijl
+ * Author URI: https://www.linkedin.com/in/dani-van-der-mijl/
+ * License: GPL2
+ */
+?>
